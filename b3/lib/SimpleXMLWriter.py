3577e1165f28f6b4f8e687e507df76312f57ac8d
John Wagenleitner <john.wagenleitner@gmail.com>
2018-10-11 21:06:24 -0700

    fix for PY3

--- b3/lib/SimpleXMLWriter.py
+++ b3/lib/SimpleXMLWriter.py
@@ -89,17 +89,10 @@
 
 import re
 
-try:
-    isinstance("", str)
-except NameError:
-    def encode(s, encoding):
-        # 1.5.2: application must use the right encoding
-        return s
-    _escape = re.compile(r"[&<>\"\x80-\xff]+") # 1.5.2
-else:
-    def encode(s, encoding):
-        return s.encode(encoding)
-    _escape = re.compile(eval(r'u"[&<>\"\u0080-\uffff]+"'))
+def encode(s, encoding):
+    # 1.5.2: application must use the right encoding
+    return s
+_escape = re.compile(r"[&<>\"\x80-\xff]+") # 1.5.2
 
 def encode_entity(text, pattern=_escape):
     # map reserved and non-ascii characters to numerical entities
@@ -107,7 +100,7 @@ def encode_entity(text, pattern=_escape):
         out = []
         for char in m.group():
             out.append("&#%d;" % ord(char))
-        return out.join("")
+        return "".join(out)
     return encode(pattern.sub(escape_entities, text), "ascii")
 
 del _escape
@@ -166,7 +159,7 @@ class XMLWriter:
             self.__write(">")
             self.__open = 0
         if self.__data:
-            data = self.__data.join("")
+            data = "".join(self.__data)
             self.__write(escape_cdata(data, self.__encoding))
             self.__data = []
 
